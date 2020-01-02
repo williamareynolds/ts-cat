4a02fcfd963c764fbad9782be1b44ed7fbc454ae
William Reynolds <williamareynolds@icloud.com>
2020-01-01 18:37:37 -0600

    fix(interface): corrects the type of the chain function and method
    The chain method and function were erroneously accepting a function wrapped in structure, as in ap.
    
    BREAKING CHANGE: The type of Chain1 and ChainS1 have changed such that their first argument is no
    longer wrapped in structure.

--- src/Chain.ts
+++ src/Chain.ts
@@ -2,9 +2,9 @@ import { Type, URIs } from './HKT'
 import { Apply1, ApplyS1 } from './Apply'
 
 export interface Chain1<F extends URIs, A> extends Apply1<F, A> {
-  chain: <B>(mab: Type<F, (a: A) => Type<F, B>>) => Type<F, B>
+  chain: <B>(fa: (a: A) => Type<F, B>) => Type<F, B>
 }
 
 export interface ChainS1<F extends URIs> extends ApplyS1<F> {
-  chain: <A, B>(mab: Type<F, (a: A) => Type<F, B>>, ma: Type<F, A>) => Type<F, B>
+  chain: <A, B>(fa: (a: A) => Type<F, B>, ma: Type<F, A>) => Type<F, B>
 }
