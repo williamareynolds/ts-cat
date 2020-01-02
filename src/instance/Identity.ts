49bad0bf852853fe6334fbc48201e7ada006159b
William Reynolds <williamareynolds@icloud.com>
2020-01-01 18:58:17 -0600

    fix(instance): correct the chain implementation for Identity
    BREAKING CHANGE: The implementation of chain for Identity was corrected to match the implementation
    in the corrected Chain interfaces.

--- src/instance/Identity.ts
+++ src/instance/Identity.ts
@@ -9,41 +9,88 @@ declare module '../HKT' {
 export const URI = 'Identity'
 export type URI = typeof URI
 
+/**
+ * A simple wrapper around a value which provides no special behavior.
+ *
+ * Identity is valuable in cases where some function expects an instance of Functor, Monad, or
+ * otherwise, but the data to pass needs no special behavior (as may be provided by something like
+ * Maybe).
+ */
 export class Identity<A> implements Monad1<URI, A> {
-  constructor(readonly value: A, readonly tag: 'Identity' = 'Identity') {}
+  /** @param value A value of any type to wrap in Identity */
+  constructor(readonly value: A) {}
 
+  /**
+   * Apply a function to the value wrapped by Identity.
+   *
+   * @param f A unary function.
+   */
   map<B>(f: (a: A) => B): Identity<B> {
     return identity.of(f(this.value))
   }
 
+  /**
+   * Apply a function wrapped in Identity to the value wrapped by this Identity.
+   *
+   * @param fab An Identity wrapping a unary function.
+   */
   ap<B>(fab: Identity<(a: A) => B>): Identity<B> {
     return identity.of(fab.value(this.value))
   }
 
-  chain<B>(mab: Identity<(a: A) => Identity<B>>): Identity<B> {
-    return identity.of(mab.value(this.value).value)
+  /**
+   * A monadic bind. Apply a function which returns an Identity without adding additional structure.
+   *
+   * @param fa A function which returns an Identity.
+   */
+  chain<B>(fa: (a: A) => Identity<B>): Identity<B> {
+    return fa(this.value)
   }
 }
 
+/**
+ * Create a new Identity object.
+ *
+ * @param a Any value to wrap with Identity.
+ */
 const of = <A>(a: A): Identity<A> => {
   return new Identity(a)
 }
 
-/** A common alias of `of` */
+/** A common alias of [[of]] */
 const pure = of
 
+/**
+ * Apply a function to the value within an Identity.
+ *
+ * @param f A unary function.
+ * @param fa An Identity containing the value to which the function will be applied.
+ */
 const map = <A, B>(f: (a: A) => B, fa: Identity<A>): Identity<B> => {
   return fa.map(f)
 }
 
+/**
+ * Apply a function wrapped by Identity to the value within another Identity.
+ *
+ * @param fab A unary function wrapped by Identity.
+ * @param fa An identity containing the value to which the function will be applied.
+ */
 const ap = <A, B>(fab: Identity<(a: A) => B>, fa: Identity<A>): Identity<B> => {
   return fa.ap(fab)
 }
 
-const chain = <A, B>(mab: Identity<(a: A) => Identity<B>>, ma: Identity<A>): Identity<B> => {
-  return ma.chain(mab)
+/**
+ * Apply a function which returns Identity without adding structure.
+ *
+ * @param fa A unary function which returns an Identity
+ * @param ma An identity containing the value to which the function will be applied.
+ */
+const chain = <A, B>(fa: (a: A) => Identity<B>, ma: Identity<A>): Identity<B> => {
+  return ma.chain(fa)
 }
 
+/** The set of static identity functions which can be applied with Identity */
 export const identity: MonadS1<URI> = {
   map,
   ap,
