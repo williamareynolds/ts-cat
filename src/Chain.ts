5c24fb48112e5e1a287022e2a42640aa7e8f5e00
William Reynolds <williamareynolds@icloud.com>
2020-01-01 21:06:27 -0600

    fix(interface): make members readonly
    BREAKING CHANGE: Some functions on type-class interfaces were marked as readonly.

--- src/Chain.ts
+++ src/Chain.ts
@@ -2,9 +2,9 @@ import { Type, URIs } from './HKT'
 import { Apply1, ApplyS1 } from './Apply'
 
 export interface Chain1<F extends URIs, A> extends Apply1<F, A> {
-  chain: <B>(fa: (a: A) => Type<F, B>) => Type<F, B>
+  readonly chain: <B>(fa: (a: A) => Type<F, B>) => Type<F, B>
 }
 
 export interface ChainS1<F extends URIs> extends ApplyS1<F> {
-  chain: <A, B>(fa: (a: A) => Type<F, B>, ma: Type<F, A>) => Type<F, B>
+  readonly chain: <A, B>(fa: (a: A) => Type<F, B>, ma: Type<F, A>) => Type<F, B>
 }
--- src/Monad.ts
+++ src/Monad.ts
@@ -6,5 +6,5 @@ import { ApplicativeS1 } from './Applicative'
 export interface Monad1<M extends URIs, A> extends Apply1<M, A>, Chain1<M, A> {}
 
 export interface MonadS1<M extends URIs> extends ApplicativeS1<M>, ChainS1<M> {
-  pure: <A>(a: A) => Type<M, A>
+  readonly pure: <A>(a: A) => Type<M, A>
 }
