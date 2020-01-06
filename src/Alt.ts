801d1eb1828ff39f92432166ae0a3af9d0bd7f26
William Reynolds <williamareynolds@icloud.com>
2020-01-06 01:27:08 -0600

    fix(interfaces): add readonly to interface members for Alt and Plus
    BREAKING CHANGE: In 5.2.0, Alt had an interface which didn't make alt readonly. It is now readonly.

--- src/Alt.ts
+++ src/Alt.ts
@@ -1,9 +1,9 @@
 import { Type, URIs } from './HKT'
 
 export interface Alt1<F extends URIs, A> {
-  alt: (fa: Type<F, A>) => Type<F, A>
+  readonly alt: (fa: Type<F, A>) => Type<F, A>
 }
 
 export interface AltS1<F extends URIs> {
-  alt: <A>(f2: Type<F, A>, f1: Type<F, A>) => Type<F, A>
+  readonly alt: <A>(f2: Type<F, A>, f1: Type<F, A>) => Type<F, A>
 }
--- src/Plus.ts
+++ src/Plus.ts
@@ -2,9 +2,9 @@ import { Type, URIs } from './HKT'
 import { Alt1, AltS1 } from './Alt'
 
 export interface Plus1<F extends URIs, A> extends Alt1<F, A> {
-  zero: () => Type<F, A>
+  readonly zero: () => Type<F, A>
 }
 
 export interface PlusS1<F extends URIs> extends AltS1<F> {
-  zero: <A>(fa: Type<F, A>) => Type<F, A>
+  readonly zero: <A>(fa: Type<F, A>) => Type<F, A>
 }
