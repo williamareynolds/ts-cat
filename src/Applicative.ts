27a64f3194d98715b7d834189e9b076c5b7ab01c
William Reynolds <williamareynolds@icloud.com>
2020-01-05 22:03:01 -0600

    feat(applicative): add stub for Applicative class interface
    Since Typescript doesn't allow static member interfaces, Applicative1 can't really enforce an
    instance, but at the very least the comment may be a good indicator to developers of what should be
    done with it. Monad now extends it. This should also be useful for creating abstract functions which
    create or return Applicatives.
    
    relates #123

--- src/Applicative.ts
+++ src/Applicative.ts
@@ -1,5 +1,14 @@
 import { Type, URIs } from './HKT'
-import { ApplyS1 } from './Apply'
+import { Apply1, ApplyS1 } from './Apply'
+
+export interface Applicative1<F extends URIs, A> extends Apply1<F, A> {
+  /*
+  Typescript cannot yet handle static interface declarations.
+  You MUST implement a static function on the class as follows:
+
+  readonly static of: <A>(a: A) => Type<F, A>
+  */
+}
 
 export interface ApplicativeS1<F extends URIs> extends ApplyS1<F> {
   readonly of: <A>(a: A) => Type<F, A>
--- src/Monad.ts
+++ src/Monad.ts
@@ -1,9 +1,16 @@
 import { Chain1, ChainS1 } from './Chain'
 import { Type, URIs } from './HKT'
-import { Apply1 } from './Apply'
-import { ApplicativeS1 } from './Applicative'
+import { Applicative1, ApplicativeS1 } from './Applicative'
 
-export interface Monad1<M extends URIs, A> extends Apply1<M, A>, Chain1<M, A> {}
+export interface Monad1<M extends URIs, A> extends Applicative1<M, A>, Chain1<M, A> {
+  /*
+  Typescript cannot yet handle static interface declarations.
+  You MUST implement static functions on the class as follows:
+
+  readonly static of: <A>(a: A) => Type<M, A> // From Applicative1
+  readonly static pure: <A>(a: A) => Type<M, A>
+  */
+}
 
 export interface MonadS1<M extends URIs> extends ApplicativeS1<M>, ChainS1<M> {
   readonly pure: <A>(a: A) => Type<M, A>
